dd3ade166797d99f41272e8bce3da9c312b356c9
NtaleGrey <Shadikntale@gmail.com>
2019-02-07 17:41:28 +0300

    ft(signup): enable a user to signup This commit delivers a feature that enables a user to signup
    [Delivers 163722350]

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,7 +6,9 @@ import store from "./store";
 import Signup from "./views/signUpView/signUpView";
 import AppPage from "./components/homeComponent/homeComponent";
 import NavBar from "./components/NavBar/NavBar";
+import NotFound from './components/NotFound/NotFound'
 import 'react-toastify/dist/ReactToastify.min.css'
+
 const Routes = () => (
   <Router>
     <Provider store={store}>
@@ -15,6 +17,7 @@ const Routes = () => (
         <Switch>
         <Route path="/" component={AppPage} exact />
         <Route path="/signup" component={Signup} exact />
+        <Route path='*' component={NotFound} exact />
       </Switch>
     </Provider>
   </Router>
